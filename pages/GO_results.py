273e3f228f7bfe85883e2e9c58e88b9d02cfc909
DLRX <dodo.leroux@outlook.com>
2025-06-18 11:29:25 +0200

    Update GO_results.py

--- pages/GO_results.py
+++ pages/GO_results.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import cafaeval
+from cafaeval.evaluation import cafa_eval
+import numpy as np 
+from tqdm import tqdm
 
-st.write("We work on it...")
+obo_file = st.text_input("Entrez le chemin d'accès du fichier (.obo & without "")")
+pred_dir = st.text_input("Entrez le chemin d'accès du dossier de prédictions (without "")")
+gt_file = st.text_input("Entrez le chemin d'accès du fichier ground_truth (without "")")
+
+
+
+df, df_b = cafa_eval(obo_file, pred_dir, gt_file)
+
+
+st.write(df_b['f'])
