e198cc9d5fefa9acba153322ba6d951a18aa35df
DLRX <dodo.leroux@outlook.com>
2025-06-18 14:07:04 +0200

    Update GO_results.py

--- pages/GO_results.py
+++ pages/GO_results.py
@@ -6,12 +6,16 @@ from tqdm import tqdm
 
 
 
-obo_file = fr"{st.text_input("Entrez le chemin d'accès du fichier (.obo & without "")")}
-pred_dir = fr"{st.text_input("Entrez le chemin d'accès du dossier de prédictions (without "")")}
-gt_file = fr"{st.text_input("Entrez le chemin d'accès du fichier ground_truth (without "")")}
+obo_file = st.text_input("Entrez le chemin d'accès du fichier (.obo & without "")")
+pred_dir = st.text_input("Entrez le chemin d'accès du dossier de prédictions (without "")")
+gt_file = st.text_input("Entrez le chemin d'accès du fichier ground_truth (without "")")
 
 
 if obo_file and pred_dir and gt_file:
+    obo_file = fr"{obo_file}"
+    pred_dir = fr"{pred_dir}"
+    gt_file = fr"{gt_file}"
+
     df, df_b = cafa_eval(obo_file, pred_dir, gt_file)
 
 
