d76a36230049e4d2c899f744f762bf92e829166d
DLRX <dodo.leroux@outlook.com>
2025-06-18 14:08:28 +0200

    Update GO_results.py

--- pages/GO_results.py
+++ pages/GO_results.py
@@ -3,18 +3,23 @@ import cafaeval
 from cafaeval.evaluation import cafa_eval
 import numpy as np 
 from tqdm import tqdm
+import os
 
 
 
-obo_file = st.text_input("Entrez le chemin d'accès du fichier (.obo & without "")")
-pred_dir = st.text_input("Entrez le chemin d'accès du dossier de prédictions (without "")")
-gt_file = st.text_input("Entrez le chemin d'accès du fichier ground_truth (without "")")
+obo_file = st.text_input("Entrez le chemin d'accès du fichier (.obo & without \"\")")
+pred_dir = st.text_input("Entrez le chemin d'accès du dossier de prédictions (without \"\")")
+gt_file = st.text_input("Entrez le chemin d'accès du fichier ground_truth (without \"\")")
+
+if obo_file:
+    obo_file = os.path.abspath(obo_file)
+if pred_dir:
+    pred_dir = os.path.abspath(pred_dir)
+if gt_file:
+    gt_file = os.path.abspath(gt_file)
 
 
 if obo_file and pred_dir and gt_file:
-    obo_file = fr"{obo_file}"
-    pred_dir = fr"{pred_dir}"
-    gt_file = fr"{gt_file}"
 
     df, df_b = cafa_eval(obo_file, pred_dir, gt_file)
 
