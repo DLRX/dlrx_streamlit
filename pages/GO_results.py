674b2a85255304bc2907b0b9c75dba3abd5f0e7a
DLRX <dodo.leroux@outlook.com>
2025-06-18 14:11:40 +0200

    Update GO_results.py

--- pages/GO_results.py
+++ pages/GO_results.py
@@ -8,20 +8,16 @@ import os
 
 
 obo_file = st.text_input("Entrez le chemin d'accès du fichier (.obo & without \"\")")
-pred_dir = st.text_input("Entrez le chemin d'accès du dossier de prédictions (without \"\")")
-gt_file = st.text_input("Entrez le chemin d'accès du fichier ground_truth (without \"\")")
 
 if obo_file:
-    obo_file = os.path.abspath(obo_file)
-if pred_dir:
-    pred_dir = os.path.abspath(pred_dir)
-if gt_file:
-    gt_file = os.path.abspath(gt_file)
-
+    pred_dir = st.text_input("Entrez le chemin d'accès du dossier de prédictions (without \"\")")
+    if pred_dir:
+        gt_file = st.text_input("Entrez le chemin d'accès du fichier ground_truth (without \"\")")
 
 if obo_file and pred_dir and gt_file:
+    obo_file = os.path.abspath(obo_file)
+    pred_dir = os.path.abspath(pred_dir)
+    gt_file = os.path.abspath(gt_file)
 
     df, df_b = cafa_eval(obo_file, pred_dir, gt_file)
-
-
     st.write(df_b['f'])
